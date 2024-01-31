1f44e4ccf36a1825f4accb653b95966f1155d7d0
Viktor Benei <viktorbenei@bitrise.io>
2024-01-31 16:33:36 +0100

    Update step.sh - fixing the annotation

--- step.sh
+++ step.sh
@@ -1,25 +1,33 @@
-#!/bin/bash
-set -ex
+#!/usr/bin/env bash
 
+# 'read' has to be before 'set -e'
+read -r -d '' UNAVAILABLE_MESSAGE << EOF_MSG
+Bitrise Build Cache is not activated in this build.
 
-UNAVAILABLE_MESSAGE=$(cat <<-END
 You have added the **Activate Bitrise Build Cache for Bazel** add-on step to your workflow.
 
-However, it has not been activated for this workspace yet. Please contact [support@bitrise.io](mailto:support@bitrise.io) to activate it.
-Build cache is not activated in this build.
-END
-)
+However, you don't have an activate Bitrise Build Cache Trial or Subscription for the current workspace yet.
 
+You can activate a Trial at [app.bitrise.io/build-cache](https://app.bitrise.io/build-cache),
+or contact us at [support@bitrise.io](mailto:support@bitrise.io) to activate it.
+EOF_MSG
+
+set -eo pipefail
+
+echo "Checking whether Bitrise Build Cache is activated for this workspace ..."
 if [ "$BITRISEIO_BUILD_CACHE_ENABLED" != "true" ]; then
   printf "\n%s\n" "$UNAVAILABLE_MESSAGE"
+  set -x
   bitrise plugin install https://github.com/bitrise-io/bitrise-plugins-annotations.git
-  bitrise :annotation annotate "$UNAVAILABLE_MESSAGE" --style error || {
+  bitrise :annotations annotate "$UNAVAILABLE_MESSAGE" --style error || {
     echo "Failed to create annotation"
-    exit 0
+    exit 3
   }
-  exit 0
+  exit 2
 fi
+echo "Bitrise Build Cache is activated in this workspace, configuring the build environment ..."
 
+set -x
 
 case "${BITRISE_DEN_VM_DATACENTER}" in
 LAS1)
